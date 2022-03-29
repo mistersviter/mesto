f5886b42903cb4f34fe6d6fafd3635a81076f024
Tarabrin Evgeniy <mistersviter@gmail.com>
2022-03-30 01:21:08 +0400

    fix: fix card links

--- scripts/index.js
+++ scripts/index.js
@@ -36,27 +36,27 @@ const zoomCloseBtn = zoomContainer.querySelector('.popup__close-button');
 const initialCards = [
   {
     name: 'Брангуа Харрор Сутас',
-    link: '../images/card_image1.jpg'
+    link: './images/card_image1.jpg'
   },
   {
     name: 'Аэронас Ту',
-    link: '../images/card_image2.jpg'
+    link: './images/card_image2.jpg'
   },
   {
     name: 'Огос',
-    link: '../images/card_image3.jpg'
+    link: './images/card_image3.jpg'
   },
   {
     name: 'Иби',
-    link: '../images/card_image4.jpg'
+    link: './images/card_image4.jpg'
   },
   {
     name: 'Хорсара',
-    link: '../images/card_image5.jpg'
+    link: './images/card_image5.jpg'
   },
   {
     name: 'Вурбедиу Моно Нахл',
-    link: '../images/card_image6.jpg'
+    link: './images/card_image6.jpg'
   }
 ];
 const cardsContainer = document.querySelector('.places__cards');
