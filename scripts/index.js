a4e90a10c23b886d4e957e7192a28cdc034180cb
Tarabrin Evgeniy <mistersviter@gmail.com>
2022-04-12 02:01:36 +0400

    fix: replace cards pics and move initialCadrs array to personal js file

--- scripts/index.js
+++ scripts/index.js
@@ -33,32 +33,6 @@ const zoomImage = zoomFigure.querySelector('.popup__zoom-image');
 const zoomCaption = zoomFigure.querySelector('.popup__zoom-caption');
 
 // Карточки
-const initialCards = [
-  {
-    name: 'Брангуа Харрор Сутас',
-    link: './images/card_image1.jpg'
-  },
-  {
-    name: 'Аэронас Ту',
-    link: './images/card_image2.jpg'
-  },
-  {
-    name: 'Огос',
-    link: './images/card_image3.jpg'
-  },
-  {
-    name: 'Иби',
-    link: './images/card_image4.jpg'
-  },
-  {
-    name: 'Хорсара',
-    link: './images/card_image5.jpg'
-  },
-  {
-    name: 'Вурбедиу Моно Нахл',
-    link: './images/card_image6.jpg'
-  }
-];
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.places__cards');
 
